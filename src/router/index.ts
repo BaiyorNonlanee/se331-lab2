bf24971e8764d9d2499b4f9e32c371accd1417fa
Nonlanee <nonneenie@gmail.com>
2024-07-16 16:04:08 +0700

    03_1.8

--- src/router/index.ts
+++ src/router/index.ts
@@ -26,9 +26,10 @@ const router = createRouter({
       component: StudentView
     },
     {
-      path: '/event/123',
+      path: '/event/:id',
       name: 'event-detail',
       component: EventDetailView,
+      props: true
     }
   ]
 })
