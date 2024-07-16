43454c2e24b090a14b73347a0d8861b9cd7de378
Nonlanee <nonneenie@gmail.com>
2024-07-16 17:32:05 +0700

    03_2.8

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -34,14 +34,19 @@ const hasNextPage = computed(() =>{
   <h1>Events For Good</h1>
  
   <div class="events">
-    <EventCard v-for="event in events" :key="event.id" :event="event"/>
-    <RouterLink :to="{name: 'event-list-view', query: {page: page - 1}}" rel="prev" v-if="page != 1">
+    
+  <EventCard v-for="event in events" :key="event.id" :event="event"/>
+
+  <div class="pagination">
+   
+    <RouterLink :to="{name: 'event-list-view', query: {page: page - 1}}" rel="prev" v-if="page != 1" id="page-prev">
       Prev Page
     </RouterLink>
-    <RouterLink :to="{name: 'event-list-view', query: {page: page + 1}}" rel="next" v-if="hasNextPage">
+    <RouterLink :to="{name: 'event-list-view', query: {page: page + 1}}" rel="next" v-if="hasNextPage" id="page-next">
       Next Page
     </RouterLink>
   </div>
+</div>
 </template>
 
 <style scoped>
@@ -49,6 +54,21 @@ const hasNextPage = computed(() =>{
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.pagination{
+  display: flex;
+  width: 290px;
+}
+.pagination a{
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50
+}
+#page-prev{
+  text-align: left;
+}
+#page-next{
+  text-align: right;
 }
 </style>
 
