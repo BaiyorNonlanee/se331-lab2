0bcef682237c1e876819ea22ce8f2b5fb26b3a2e
Nonlanee <nonneenie@gmail.com>
2024-07-18 13:53:56 +0700

    03_3.2

--- src/views/event/EventRegisterView.vue
+++ src/views/event/EventRegisterView.vue
@@ -1,33 +1,17 @@
 <script setup lang="ts">
 import {ref} from 'vue'
 import {type EventItem} from '@/type'
-import EventService from '@/services/EventService'
-const event = ref <EventItem | null>(null)
+import type { PropType } from 'vue';
 
 const props = defineProps({
-    id: String
-})
-
-EventService.getEventById(Number(props.id)).then((response) =>{
-    event.value = response.data
-}).catch(error => {
-    console.log(error)
+    event:{
+        type: Object as PropType<EventItem>,
+            require: true
+    }
 })
 </script>
 <template>
-    <div v-if="event">
-        <h1>{{ event.title }}</h1>
-        <div class="nav">
-            <router-link :to="{ name: 'event-detail' , params:{id}}">
-                >Details 
-            </router-link> |
-            <router-link :to="{ name: 'event-register' , params:{id}}">
-                >Register 
-            </router-link> |
-            <router-link :to="{ name: 'event-edit' , params:{id}}">
-                >Edit
-            </router-link> 
-        </div>
+
        <p>Registration from here</p>
-    </div>
+ 
 </template>
