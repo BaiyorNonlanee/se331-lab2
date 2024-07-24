3e51fc9c2a38f7921b8a90c10f09770e0e048b5a
Nonlanee <nonneenie@gmail.com>
2024-07-25 00:32:54 +0700

    04_2.2

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -2,10 +2,9 @@
 import EventCard from '@/components/EventCard.vue'
 import {type Event} from '@/types'
 import { useRoute, RouterLink } from 'vue-router'
-// import type { AxiosResponse } from 'axios';
 import {ref, onMounted, computed, watchEffect } from 'vue'
 import EventService from '@/services/EventService'
-
+import nProgress from 'nprogress'
 
 const events = ref<Event[]>(null)
 const totalEvents = ref<number>(0)
@@ -39,6 +38,7 @@ watchEffect(() => {
   events.value = []
   
   // Fetch events based on page size and current page
+  nProgress.start()
   EventService.getEvents(pageSize.value, page.value)
     .then((response) => {
       events.value = response.data
@@ -46,6 +46,9 @@ watchEffect(() => {
     })
     .catch((error) => {
       console.error('There was an error!', error)
+    })
+    .finally(() => {
+      nProgress.done()
     })
 })
 
