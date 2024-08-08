537569a59ef8adefdd53e9da2ff5393890d6fee0
Nonlanee <nonneenie@gmail.com>
2024-08-08 14:31:02 +0700

    edit student

--- src/views/StudentView.vue
+++ src/views/StudentView.vue
@@ -1,36 +1,25 @@
 <script setup lang="ts">
 import StudentCard from '@/components/StudentCard.vue'
-import Student from '@types/Student'
-import { ref,onMounted } from 'vue'
-import StudentService from '@/services/StudentService.ts'
+import type { Student } from '@/types'
+import { ref, onMounted } from 'vue'
+import StudentService from '@/services/StudentService'
 
-const students = ref<Student[]>(null)
+const students = ref<Student[]>([])
 
-onMounted(() =>{
+onMounted(() => {
   StudentService.getEvents()
-  .then((response) =>{
-    students.value = response.data
-  })
-  .catch((error) =>{
-    console.error('There was an error!', error)
-  })
+    .then((response) => {
+      students.value = response.data
+    })
+    .catch((error) => {
+      console.error('There was an error!', error)
+    })
 })
 </script>
 
 <template>
   <h1>Student</h1>
-  <!-- new element -->
   <div class="flex flex-col items-center">
     <StudentCard v-for="student in students" :key="student.id" :student="student"/>
   </div>
 </template>
-
-<!-- <style scoped>
-.events{
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-</style> -->
-
-
